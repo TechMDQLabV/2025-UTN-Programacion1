b34d069abefa253010a783910896a64abadd46fe
Sergio Garguir <sergiogarguir@gmail.com>
2025-03-11 20:43:06 -0300

    primeras funciones void

--- primerProyecto/main.c
+++ primerProyecto/main.c
@@ -1,17 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <conio.h>
 
 int main()
 {
     char opcion;
 
     do{
-        printf("\n\t\t\t Menu de Opciones\n");
-        printf("\n1 - Tabla ASCII");
-        printf("\n2 - Mostrar dir");
-        printf("\n3 - Ejercicio 2");
-        printf("\n4 - Ejercicio 3");
-        printf("\nESC para salir ...");
+        opcionesMenu();
 
         opcion = getch();
 
@@ -20,26 +16,38 @@ int main()
                 printf("\n Bye Bye");
                 break;
             case 49:
-                printf("\t\t<<< Tabla de Caracteres ASCII >>>\n");
-                for(int i=0;i<256;i++){
-                    printf("nro: %d - %c | ", i,i);
-                    if(i%5==0){
-                        printf("\n");
-                    }
-                }
+                codigoAscii();
                 break;
             case 50:
                 printf("\t\t\t Archivos de la carpeta actual");
                 system("dir");
         }
-        system("pause");
+        Sleep(1500);
         system("cls");
     }while(opcion!=27);
 
-
-
-
-
-    system("pause");
     return 0;
+}
+
+void opcionesMenu(){
+    printf("\n\t\t\t Menu de Opciones\n");
+    printf("\n1 - Tabla ASCII");
+    printf("\n2 - Mostrar dir");
+    printf("\n3 - Ejercicio 2");
+    printf("\n4 - Ejercicio 3");
+    printf("\nESC para salir ...");
+}
+
+int suma(int uno, int dos){
+    return uno + dos;
+}
+
+void codigoAscii(){
+    printf("\t\t<<< Tabla de Caracteres ASCII >>>\n");
+    for(int i=0;i<256;i++){
+        printf("nro: %d - %c | ", i,i);
+        if(i%5==0){
+            printf("\n");
+        }
+    }
 }
