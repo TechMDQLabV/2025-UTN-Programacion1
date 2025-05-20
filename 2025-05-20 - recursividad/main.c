81e34aa065d432b68c903158795a6ae1da595c68
Sergio Garguir <sergiogarguir@gmail.com>
2025-05-20 19:15:41 -0300

    recursividad

---
+++ 2025-05-20 - recursividad/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("\n Factorial 5: %d", factorial(5));
+    printf("\n Factorial 5: %d", factorial1(5));
+    printf("\n Factorial 5: %d", factorialTernario(5));
+    return 0;
+}
+
+int factorial(int x){
+    int rta;
+    if(x==0){
+        rta=1;
+    }else{
+        rta = x * factorial(x-1);
+    }
+    return rta;
+}
+
+int factorial1(int x){
+    int rta = 1;
+    if(x>0){
+        rta = x * factorial1(x-1);
+    }
+    return rta;
+}
+
+int factorialTernario(int x){
+    return (x>0) ? x * factorialTernario(x-1) : 1;
+}
